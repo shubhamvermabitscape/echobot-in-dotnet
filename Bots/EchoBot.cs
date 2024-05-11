23dba859a8e1f7fb58b6ad47f5535af244ef8600
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-05-11 17:50:31 +0530

    initial commit

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,41 @@
+﻿// Generated with EchoBot .NET Template version v4.22.0
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Rest;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            string userQuery = turnContext.Activity.Text;
+            var gptResponse = await GPTResponseAsync(userQuery);
+            await turnContext.SendActivityAsync(MessageFactory.Text(gptResponse, gptResponse), cancellationToken);
+        }
+
+        private async Task<string> GPTResponseAsync(string userQuery)
+        {
+            return userQuery;
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
